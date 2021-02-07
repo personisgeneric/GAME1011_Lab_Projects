ea9486263face8e3aefcfb98a1338893ff90a266
George <geo_gr@rogers.com>
2021-02-07 13:13:34 -0500

    Added Special Abilities for Weapons

--- GAME1011_Lab2/Weapon.h
+++ GAME1011_Lab2/Weapon.h
@@ -8,17 +8,20 @@ private:
 	std::string m_name;
 	std::string m_description;
 	int m_damage;
+	std::string m_specialAbility;
 public:
 
 	//Setters
 	void setName(std::string n) { m_name = n; }
 	void setDesc(std::string d) { m_description = d; }
 	void setDamage(int d) { m_damage = d; }
+	void setAbility();
 
 	//Getters
 	std::string getName() { return m_name; }
 	std::string getDesc() { return m_description; }
 	int getDamage() { return m_damage; }
+	std::string getAbility() { return m_specialAbility; }
 
 };
 
